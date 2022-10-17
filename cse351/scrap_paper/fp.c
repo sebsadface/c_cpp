8c778e44c3c9b6d8bada903f9f59b533fa28ef81
sebsadface <ll57@cs.washington.edu>
2022-10-17 12:38:46 -0700

    fp.c update

---
+++ cse351/scrap_paper/fp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  int count = 0;
+  float cap = 16777216;
+
+  for (float i = 0; i < cap; i++) {
+    count++;
+  }
+
+  printf("This iterated %d times\n", count);
+
+  return 0;
+}

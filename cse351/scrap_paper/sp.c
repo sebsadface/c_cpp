4db06a0fbb3b31600d619fed0a3297f503d5d76c
Sebastian Liu <sebsadface@gmail.com>
2022-11-03 16:02:28 -0700

    update

--- cse351/scrap_paper/sp.c
+++ cse351/scrap_paper/sp.c
@@ -2,11 +2,10 @@
 
 int main(int argc, char *argv[])
 {
-    // short *pairing;
-    // *pairing = 0x0F12;
-    short pairing = 0x0F12;
+    short *pairing;
+    *pairing = 0x0F12;
 
-    printf("The score is %i \n", (signed int)((pairing & 0x0000000000000F00) >> 8));
+    printf("The score is %d \n", ((signed int)*pairing << 20) >> 28);
 
     return 0;
 }

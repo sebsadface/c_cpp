617655d60bf0a38814c48b48046e4dc40f0a1c38
Sebastian Liu <sebsadface@gmail.com>
2022-11-04 18:51:19 -0700

    update

--- cse351/scrap_paper/sp.c
+++ cse351/scrap_paper/sp.c
@@ -2,10 +2,13 @@
 
 int main(int argc, char *argv[])
 {
-    short *pairing;
-    *pairing = 0x0F12;
+    short i = 0x0A12;
+    short *pairing = &i;
+    char *str = "3";
 
     printf("The score is %d \n", ((signed int)*pairing << 20) >> 28);
+    printf("The string is %s \n", str);
+    printf("The address of the string is %d \n", *str);
 
     return 0;
 }

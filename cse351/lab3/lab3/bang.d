5c1f18f83b83177c8e0361b04fbeca82dad3e0c2
sebsadface <ll57@cs.washington.edu>
2022-11-11 16:01:19 -0800

    bang update

---
+++ cse351/lab3/lab3/bang.d
@@ -0,0 +1,13 @@
+
+bang.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 b8 a1 b3 83 5a 2c 	movabs $0x1d56a22c5a83b3a1,%rax
+   7:	a2 56 1d 
+   a:	48 89 04 25 08 23 60 	mov    %rax,0x602308
+  11:	00 
+  12:	68 20 10 40 00       	pushq  $0x401020
+  17:	c3                   	retq   

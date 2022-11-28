37e3e6113033ccdda00101574ecc033869e582b7
Sebastian Liu <sebsadface@gmail.com>
2022-11-27 22:32:07 -0800

    trans.c modify

--- cse351/lab4/lab4/trans.c
+++ cse351/lab4/lab4/trans.c
@@ -27,16 +27,16 @@ int is_transpose(int M, int N, int A[M][N], int B[N][M]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[M][N], int B[N][M])
 {
-    int array[8], i, j, k, l, o;
     if (M == 32 && N == 32)
     {
+        int array[8], i, j, k, l;
         for (i = 0; i < M; i += 8)
         {
             for (j = 0; j < N; j += 8)
             {
                 for (k = 0; k < 8; k++)
                 {
-                    array[o] = A[i + k][j + o];
+                    array[0] = A[i + k][j];
 
                     for (l = 0; l < 8; l++)
                     {
@@ -48,16 +48,14 @@ void transpose_submit(int M, int N, int A[M][N], int B[N][M])
     }
     else
     {
+        int array[4], i, j, k, l;
         for (i = 0; i < M; i += 4)
         {
             for (j = 0; j < N; j += 4)
             {
                 for (k = 0; k < 4; k++)
                 {
-                    for (o = 0; o < 4; o++)
-                    {
-                        array[o] = A[i + k][j + o];
-                    }
+                    array[0] = A[i + k][j];
 
                     for (l = 0; l < 4; l++)
                     {

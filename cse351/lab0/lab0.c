feb66ecba77f9b7330e8a5549572175629d60113
Sebastian Liu <sebsadface@gmail.com>
2022-12-05 01:37:17 -0800

    lab0 update

--- cse351/lab0/lab0.c
+++ cse351/lab0/lab0.c
@@ -266,7 +266,7 @@ void part5()
     // argument.
     // sizeof(Scores) gives the size of the Scores struct in
     // bytes (which is 16), so sizeof(Scores) * 5 is 80.
-    void *new = malloc(16);
+    void *new = malloc(33);
     printf("ptr new = %p\n", new);
     Scores *class_grades = (Scores *)malloc(sizeof(Scores) * 5);
     printf("ptr grade = %p\n", class_grades);

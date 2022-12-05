cf711e4d7f2a6f691d2508779e7cf4e9375402ce
Sebastian Liu <sebsadface@gmail.com>
2022-12-05 01:42:35 -0800

    lab0 update

--- cse351/lab0/lab0.c
+++ cse351/lab0/lab0.c
@@ -266,10 +266,10 @@ void part5()
     // argument.
     // sizeof(Scores) gives the size of the Scores struct in
     // bytes (which is 16), so sizeof(Scores) * 5 is 80.
-    void *new = malloc(33);
+    void *new = malloc(32);
     printf("ptr new = %p\n", new);
     Scores *class_grades = (Scores *)malloc(sizeof(Scores) * 5);
-    printf("ptr grade = %p\n", class_grades);
+    printf("diff = %X\n", (int)class_grades - (int)new);
     fillArray((int *)class_grades, 5 * 4);
 
     // Q5: Once we are finished with malloc-ed memory, we

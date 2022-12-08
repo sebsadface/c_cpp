3702c77d967cd3b5f81d27e32914ed3a57a9a87e
Sebastian Liu <sebsadface@gmail.com>
2022-12-07 22:24:17 -0800

    garbage collector update

--- cse351/lab5/lab5/mm-gc.c
+++ cse351/lab5/lab5/mm-gc.c
@@ -2,8 +2,8 @@
  * CSE 351 Lab 5 (Dynamic Storage Allocator)
  * Extra credit: Implementing garbage collection
  *
- * Name(s):  
- * NetID(s): 
+ * Name(s):
+ * NetID(s):
  *
  * NOTES:
  *  - This requires mm_malloc and mm_free to be working correctly, so don't
@@ -12,69 +12,73 @@
  */
 #include "mm.c"
 
-
 // The tag to indicate that a block is marked.
 #define TAG_MARKED 4
 
 // Forward function declarations
 static void sweep(void);
-static int is_pointer(void* ptr);
-static void mark(void* ptr);
-
+static int is_pointer(void *ptr);
+static void mark(void *ptr);
 
 /* A modified version of examine_heap() to include TAG_MARKED. */
-static void examine_heap_gc() {
-  block_info* block;
+static void examine_heap_gc()
+{
+  block_info *block;
 
   // print to stderr so output isn't buffered and not output if we crash
-  fprintf(stderr, "FREE_LIST_HEAD: %p\n", (void*) FREE_LIST_HEAD);
+  fprintf(stderr, "FREE_LIST_HEAD: %p\n", (void *)FREE_LIST_HEAD);
 
-  for (block = (block_info*) UNSCALED_POINTER_ADD(mem_heap_lo(), WORD_SIZE);  // first block on heap
-       SIZE(block->size_and_tags) != 0 && (void*) block < mem_heap_hi();
-       block = (block_info*) UNSCALED_POINTER_ADD(block, SIZE(block->size_and_tags))) {
+  for (block = (block_info *)UNSCALED_POINTER_ADD(mem_heap_lo(), WORD_SIZE); // first block on heap
+       SIZE(block->size_and_tags) != 0 && (void *)block < mem_heap_hi();
+       block = (block_info *)UNSCALED_POINTER_ADD(block, SIZE(block->size_and_tags)))
+  {
 
     // print out common block attributes
     fprintf(stderr, "%p: %ld %ld %ld %ld\t",
-            (void*) block,
+            (void *)block,
             SIZE(block->size_and_tags),
             block->size_and_tags & TAG_MARKED,
             block->size_and_tags & TAG_PRECEDING_USED,
             block->size_and_tags & TAG_USED);
 
     // and allocated/free specific data
-    if (block->size_and_tags & TAG_USED) {
+    if (block->size_and_tags & TAG_USED)
+    {
       fprintf(stderr, "ALLOCATED\n");
-    } else {
+    }
+    else
+    {
       fprintf(stderr, "FREE\tnext: %p, prev: %p\n",
-              (void*) block->next,
-              (void*) block->prev);
+              (void *)block->next,
+              (void *)block->prev);
     }
   }
   fprintf(stderr, "END OF HEAP\n\n");
 }
 
-
 /*
  * This will determine if the given pointer points to the beginning
  * of the payload of a block which is allocated.
  */
-static int is_pointer(void* ptr) {
-  size_t* cur_block = (size_t*) UNSCALED_POINTER_ADD(mem_heap_lo(), WORD_SIZE);
-  size_t* heap_footer = (size_t*) UNSCALED_POINTER_SUB(mem_heap_hi(), WORD_SIZE - 1);
+static int is_pointer(void *ptr)
+{
+  size_t *cur_block = (size_t *)UNSCALED_POINTER_ADD(mem_heap_lo(), WORD_SIZE);
+  size_t *heap_footer = (size_t *)UNSCALED_POINTER_SUB(mem_heap_hi(), WORD_SIZE - 1);
 
-  while (cur_block < heap_footer) {
+  while (cur_block < heap_footer)
+  {
     size_t size_and_tags = *cur_block;
-    void* payload_ptr = UNSCALED_POINTER_ADD(cur_block, WORD_SIZE);
+    void *payload_ptr = UNSCALED_POINTER_ADD(cur_block, WORD_SIZE);
     size_t allocated = size_and_tags & TAG_USED;
 
-    if (payload_ptr == ptr && allocated) {
+    if (payload_ptr == ptr && allocated)
+    {
       return 1;
     }
-    cur_block = (size_t*) UNSCALED_POINTER_ADD(cur_block, SIZE(size_and_tags));
+    cur_block = (size_t *)UNSCALED_POINTER_ADD(cur_block, SIZE(size_and_tags));
   }
   return 0;
 }
-
 
 /*
  * Mark the block pointed to by ptr. The argument ptr will point to the
@@ -82,30 +86,58 @@ static int is_pointer(void* ptr) {
  * that a block is reachable. Next go and mark all other blocks that are
  * pointed to by pointers in this block.
  */
-static void mark(void* ptr) {
-  size_t* block_header = (size_t*) UNSCALED_POINTER_SUB(ptr, WORD_SIZE);
+static void mark(void *ptr)
+{
+  size_t *block_header = (size_t *)UNSCALED_POINTER_SUB(ptr, WORD_SIZE);
+  if (!is_pointer(ptr))
+  {
+    return;
+  }
 
-  // TODO: Implement mark.
+  if (*(char *)UNSCALED_POINTER_ADD(block_header, WORD_SIZE - 3))
+  {
+    return;
+  }
+
+  *(char *)UNSCALED_POINTER_ADD(block_header, WORD_SIZE - 3) = 1;
+
+  for (int i = 0; i < SIZE(block_header); i += WORD_SIZE)
+  {
+    mark(UNSCALED_POINTER_ADD(ptr, i));
+  }
+  return;
 }
-
 
 /*
  * Sweep through the all of the allocated blocks in the heap and free all
  * that are unreachable (i.e., TAG_MARKED is unset).
  */
-static void sweep() {
-  size_t* cur_block = (size_t*) UNSCALED_POINTER_ADD(mem_heap_lo(), WORD_SIZE);
-  size_t* heap_footer = (size_t*) UNSCALED_POINTER_SUB(mem_heap_hi(), WORD_SIZE - 1);
+static void sweep()
+{
+  size_t *cur_block = (size_t *)UNSCALED_POINTER_ADD(mem_heap_lo(), WORD_SIZE);
+  size_t *heap_footer = (size_t *)UNSCALED_POINTER_SUB(mem_heap_hi(), WORD_SIZE - 1);
 
-  // TODO: Implement sweep.
+  while (cur_block < heap_footer)
+  {
+    if (*(char *)UNSCALED_POINTER_ADD(cur_block, WORD_SIZE - 3))
+    {
+      *(char *)UNSCALED_POINTER_ADD(cur_block, WORD_SIZE - 3) = 0;
+    }
+    else
+    {
+      free((block_info *)cur_block);
+    }
+    cur_block += SIZE(cur_block);
+  }
 }
 
-
 /* Run the mark-and-sweep garbage collection algorithm. */
-void mm_garbage_collect(void* rootPtrs[], int num_roots) {
+void mm_garbage_collect(void *rootPtrs[], int num_roots)
+{
   int i;
-  for (i = 0; i < num_roots; i++) {
-    void* root = rootPtrs[i];
+  for (i = 0; i < num_roots; i++)
+  {
+    void *root = rootPtrs[i];
     mark(root);
   }
   sweep();

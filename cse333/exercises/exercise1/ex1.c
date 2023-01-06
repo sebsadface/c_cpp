41ffebb48c14a82a8e52eee6a55b3c7ae9e8daf3
Sebastian Liu <sebsadface@gmail.com>
2023-01-05 23:54:23 -0800

    debuggging code added

--- cse333/exercises/exercise1/ex1.c
+++ cse333/exercises/exercise1/ex1.c
@@ -45,7 +45,7 @@ double getPi(int n)
     {
         if (i % 2 == 0)
         {
-            result += (double)(4 / (2 * i * (2 * i + 1) * (2 * i + 2)));
+            result += ((double)4) / ((double)(2 * i * (2 * i + 1) * (2 * i + 2)));
         }
         else
         {

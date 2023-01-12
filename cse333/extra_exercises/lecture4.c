7a6bcb1d0155f7139febb3bd6b210ed55ac8c62a
Sebastian Liu <sebsadface@gmail.com>
2023-01-11 20:37:13 -0800

    extra exercise updates

---
+++ cse333/extra_exercises/lecture4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef struct ee1 {
+  float x;
+  float y;
+} Point;
+
+typedef struct ee11 {
+  Point p1;
+  Point p2;
+  Point p3;
+} Triangle;
+
+float
+
+    int
+    main(int argc, char* argv[]) {}

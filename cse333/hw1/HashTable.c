5afeae5f463d71cf3aa5146958b71e5c9e31577c
Sebastian Liu <sebsadface@gmail.com>
2023-01-18 03:10:25 -0800

    bug fix: Hash Table find error

--- cse333/hw1/HashTable.c
+++ cse333/hw1/HashTable.c
@@ -256,8 +256,10 @@ bool HTIterator_Get(HTIterator *iter, HTKeyValue_t *keyvalue) {
   if ((iter->ht->num_elements == 0) || !HTIterator_IsValid(iter)) {
     return false;
   }
-
-  LLIterator_Get(iter->bucket_it, (LLPayload_t *)&keyvalue);
+  HTKeyValue_t *payload;
+  LLIterator_Get(iter->bucket_it, (LLPayload_t *)&payload);
+  payload->key = keyvalue->key;
+  payload->value = keyvalue->value;
   return true;  // you may need to change this return value
 }
 

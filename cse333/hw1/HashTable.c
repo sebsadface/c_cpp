1095903f532a8d2282f35483949886a99f1099be
Sebastian Liu <sebsadface@gmail.com>
2023-01-18 04:08:36 -0800

    bug fix: Hash Table find error

--- cse333/hw1/HashTable.c
+++ cse333/hw1/HashTable.c
@@ -144,10 +144,14 @@ bool HashTable_Insert(HashTable *table, HTKeyValue_t newkeyvalue,
     oldkeyvalue->key = oldpayload->key;
     oldkeyvalue->value = oldpayload->value;
     oldpayload->value = newcopy->value;
+
+    free(newcopy);
     return true;
   }
   LinkedList_Push(chain, (LLPayload_t)newcopy);
   table->num_elements++;
+
+  free(newcopy);
   return false;  // you may need to change this return value
 }
 

de749957f9206c0425efb95ca6d10143ec7d2641
Sebastian Liu <sebsadface@gmail.com>
2023-01-29 22:17:16 -0800

    style fix

--- cse333/exercises/exercise6/Vector.h
+++ cse333/exercises/exercise6/Vector.h
@@ -8,14 +8,28 @@
 
 class Vector {
  public:
-  Vector();                                             // constructor
-  Vector(const float x, const float y, const float z);  // constructor
-  Vector(const Vector& copyMe);                         // copy constructor
-  Vector& operator=(const Vector& rhs);      // assignment operator (=)
-  Vector& operator+=(const Vector& rhs);     // assignment operator (+=)
-  Vector& operator-=(const Vector& rhs);     // assignment operator (-=)
-  float operator*(const Vector& rhs) const;  // assignment operator (*)
-  ~Vector();                                 // destructor
+  // Default constructor (sets x_, y_, z_ to zero)
+  Vector();
+
+  // 3-argument constructor (sets x_, y_, z_ to given arguments)
+  Vector(const float x, const float y, const float z);
+
+  // Copy constructor (sets x_, y_, z_ to given vector coordinates)
+  Vector(const Vector& copyMe);
+
+  // Overload operator (=) to elementwise assignment
+  Vector& operator=(const Vector& rhs);
+
+  // Overload operator (+=) to elementwise addition
+  Vector& operator+=(const Vector& rhs);
+
+  // Overload operator (-=) to elementwise subtraction
+  Vector& operator-=(const Vector& rhs);
+
+  // Overload operator (*) to vector dot product
+  float operator*(const Vector& rhs) const;
+
+  ~Vector();  // destructor
 
   float get_x() const { return x_; }  // inline member function
   float get_y() const { return y_; }  // inline member function
--- cse333/exercises/exercise6/ex6.cc
+++ cse333/exercises/exercise6/ex6.cc
@@ -3,36 +3,81 @@
 // Name: Sebastian Liu
 // CSE Email Address: ll57@cs.washington.edu
 
-#include "Vector.h"
+#include "Vector.h"  // for Vector
 
-#include <cstdlib>
-#include <iostream>
+#include <cstdlib>   // for EXIT_SUCCESS, EXIT_FAILURE
+#include <iostream>  // for cout, cerr, endl
 
 using std::cerr;
 using std::cout;
 using std::endl;
 
-#define X_COORD 14.625
-#define Y_COORD -25.25
-#define Z_COORD 3.75
+#define X_COORD1 14.625
+#define Y_COORD1 -25.25
+#define Z_COORD1 3.75
 #define X_COORD2 34.25
 #define Y_COORD2 6.75
 #define Z_COORD2 -64.5
-#define AFTER_INCREMENT 1
+#define AFTER_INCREMENT 1  // expected coordinate values after increment
 
-bool TestCtorAndOptor();
+// Test that the default (0-argument) constructor initializes a Vector to
+// (0,0,0).
+// Returns true if tests pass, false otherwise.
+bool TestDefaultCtor();
+
+// Test that the 3-argument constructor initializes a Vector to
+// the given value (x,y,z).
+// Returns true if tests pass, false otherwise.
+bool Test3ArgCtor();
+
+// Test that the copy constructor initializes a new Vector to
+// have the same value as the given Vector.
+// Returns true if tests pass, false otherwise.
+bool TestCopyCtor();
+
+// Test that the assignment operator "=", assign the coordinate values of the
+// left hand side Vector to the right hand side Vector element-by-element.
+// Returns true if tests pass, false otherwise.
+bool TestAssignOptor();
+
+// Test that the operator "+=" updates assignments on vectors that perform
+// element-by-element addition of the Vector components.
+// Returns true if tests pass, false otherwise.
+bool TestAddOptor();
+
+// Test that the operator "-=" updates assignments on vectors that perform
+// element-by-element subtraction of the Vector components.
+// Returns true if tests pass, false otherwise.
+bool TestSubOptor();
+
+// Test that the operator "*" computes the dot product of two Vectors as a
+// float.
+// Returns true if tests pass, false otherwise.
+bool TestProductOptor();
+
+// Accepts an instance of a Vector as an argument, and increments the values
+// stored in the vector by one.
 void Increment(Vector v);
-bool VerifyAddress(Vector& vref, void* address);
-void IncrementTest(const Vector& v);
+
+// Accepts a Vector reference and a void* as arguments.
+// Returns true if the address of the aliased Vector has the same numeric value
+// as the passed-in void*, and returns false otherwise.
+bool VerifyAddress(const Vector& vref, void* address);
 
 int main(int argc, char** argv) {
-  if (!TestCtorAndOptor()) {
+  // Tests all functions defined in Vector.h.
+  if (!TestDefaultCtor() || !Test3ArgCtor() || !TestCopyCtor() ||
+      !TestAssignOptor() || !TestAddOptor() || !TestSubOptor() ||
+      !TestProductOptor()) {
     cerr << stderr << "Testing failed" << endl;
+
     return EXIT_FAILURE;
   }
 
   Vector v;
 
+  // Determine whether the Vector object v itself was passed by value or passed
+  // by reference to Increment()
   Increment(v);
   if (v.get_x() != AFTER_INCREMENT || v.get_y() != AFTER_INCREMENT ||
       v.get_z() != AFTER_INCREMENT) {
@@ -41,70 +86,115 @@ int main(int argc, char** argv) {
     cout << "Vector: pass-by-reference" << endl;
   }
 
+  // Determine whether the main() instance of Vector v is the same instance as
+  // the one visible to VerifyAddress() by comparing their addresses.
   if (VerifyAddress(v, &v)) {
     cout << "Ref: same address" << endl;
   } else {
     cout << "Ref: different address" << endl;
   }
+
+  return EXIT_SUCCESS;
 }
 
-bool TestCtorAndOptor() {
-  Vector v1;
-  if (v1.get_x() != 0 || v1.get_y() != 0 || v1.get_z() != 0) {
+bool TestDefaultCtor() {
+  const Vector v;
+
+  if (v.get_x() != 0 || v.get_y() != 0 || v.get_z() != 0) {
     cerr << stderr << " Default vector constructor failed" << endl;
     return false;
   }
+  return true;
+}
 
-  Vector v2(X_COORD, Y_COORD, Z_COORD);
-  Vector v3(X_COORD2, Y_COORD2, Z_COORD2);
-  if (v2.get_x() != X_COORD || v2.get_y() != Y_COORD || v2.get_z() != Z_COORD ||
-      v3.get_x() != X_COORD2 || v3.get_y() != Y_COORD2 ||
-      v3.get_z() != Z_COORD2) {
+bool Test3ArgCtor() {
+  const Vector v(X_COORD1, Y_COORD1, Z_COORD1);
+
+  if (v.get_x() != X_COORD1 || v.get_y() != Y_COORD1 || v.get_z() != Z_COORD1) {
     cerr << stderr << " Three-parameter vector constructor failed" << endl;
-    return false;
-  }
 
-  Vector cp(v2);
-  if (cp.get_x() != X_COORD || cp.get_y() != Y_COORD || cp.get_z() != Z_COORD) {
-    cerr << stderr << " Copy vector constructor failed" << endl;
-    return false;
-  }
-
-  v1 = v2;
-  if (v1.get_x() != X_COORD || v1.get_y() != Y_COORD || v1.get_z() != Z_COORD) {
-    cerr << stderr << " Overwritting assignment operator = failed" << endl;
-    return false;
-  }
-
-  v2 += v3;
-  if (v2.get_x() != X_COORD + X_COORD2 || v2.get_y() != Y_COORD + Y_COORD2 ||
-      v2.get_z() != Z_COORD + Z_COORD2) {
-    cerr << stderr << " Overwritting operator += failed" << endl;
-    return false;
-  }
-
-  v2 -= v3;
-  if (v2.get_x() != X_COORD || v2.get_y() != Y_COORD || v2.get_z() != Z_COORD) {
-    cerr << stderr << " Overwritting operator -= failed" << endl;
-    return false;
-  }
-
-  float dotProduct = v2 * v3;
-  if (dotProduct != v2.get_x() * v3.get_x() + v2.get_y() * v3.get_y() +
-                        v2.get_z() * v3.get_z()) {
-    cerr << stderr << " Overwritting operator * failed" << endl;
     return false;
   }
 
   return true;
 }
 
+bool TestCopyCtor() {
+  const Vector v(X_COORD1, Y_COORD1, Z_COORD1);
+  const Vector cp(v);
+
+  if (cp.get_x() != v.get_x() || cp.get_y() != v.get_y() ||
+      cp.get_z() != v.get_z()) {
+    cerr << stderr << " Copy vector constructor failed" << endl;
+    return false;
+  }
+  return true;
+}
+
+bool TestAssignOptor() {
+  Vector lhs;
+  const Vector rhs(X_COORD1, Y_COORD1, Z_COORD1);
+
+  lhs = rhs;
+
+  if (lhs.get_x() != X_COORD1 || lhs.get_y() != Y_COORD1 ||
+      lhs.get_z() != Z_COORD1) {
+    cerr << stderr << " Overwritting assignment operator = failed" << endl;
+    return false;
+  }
+  return true;
+}
+
+bool TestAddOptor() {
+  Vector lhs(X_COORD1, Y_COORD1, Z_COORD1);
+  const Vector rhs(X_COORD2, Y_COORD2, Z_COORD2);
+
+  lhs += rhs;
+
+  if (lhs.get_x() != X_COORD1 + X_COORD2 ||
+      lhs.get_y() != Y_COORD1 + Y_COORD2 ||
+      lhs.get_z() != Z_COORD1 + Z_COORD2) {
+    cerr << stderr << " Overwritting operator += failed" << endl;
+    return false;
+  }
+  return true;
+}
+
+bool TestSubOptor() {
+  Vector lhs(X_COORD1, Y_COORD1, Z_COORD1);
+  const Vector rhs(X_COORD2, Y_COORD2, Z_COORD2);
+
+  lhs -= rhs;
+
+  if (lhs.get_x() != X_COORD1 - X_COORD2 ||
+      lhs.get_y() != Y_COORD1 - Y_COORD2 ||
+      lhs.get_z() != Z_COORD1 - Z_COORD2) {
+    cerr << stderr << " Overwritting operator -= failed" << endl;
+    return false;
+  }
+  return true;
+}
+
+bool TestProductOptor() {
+  const Vector v1(X_COORD1, Y_COORD1, Z_COORD1);
+  const Vector v2(X_COORD2, Y_COORD2, Z_COORD2);
+
+  float dotProduct = v1 * v2;
+
+  if (dotProduct !=
+      X_COORD1 * X_COORD2 + Y_COORD1 * Y_COORD2 + Z_COORD1 * Z_COORD2) {
+    cerr << stderr << " Overwritting operator * failed" << endl;
+    return false;
+  }
+  return true;
+}
+
 void Increment(Vector v) {
-  Vector one(1, 1, 1);
+  const Vector one(1, 1, 1);
   v += one;
 }
 
-bool VerifyAddress(Vector& vref, void* address) {
+bool VerifyAddress(const Vector& vref, void* address) {
   if (&vref == address) {
     return true;
   } else {

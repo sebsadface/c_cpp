69722bb45a9b5a3fd2875dc7c32b21470dbad832
Sebastian Liu <sebsadface@gmail.com>
2023-02-04 21:21:14 -0800

    search shell memory error fix

--- cse333/hw2/searchshell.c
+++ cse333/hw2/searchshell.c
@@ -145,7 +145,7 @@ static int GetNextLine(FILE* f, char** ret_str) {
   }
 
   token = strtok_r(buffer, " ", &last);
-  while (token != NULL && *token != '\n') {
+  while (token != NULL) {
     ret_str[ret_len] = token;
     ret_len++;
     token = strtok_r(NULL, " ", &last);

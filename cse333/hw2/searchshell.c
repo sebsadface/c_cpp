449195e0c0a86125cfac1fd3c3016d61060b2027
Sebastian Liu <sebsadface@gmail.com>
2023-02-04 19:48:56 -0800

    search shell memory error fix

--- cse333/hw2/searchshell.c
+++ cse333/hw2/searchshell.c
@@ -90,7 +90,7 @@ static void ProcessQueries(DocTable* dt, MemIndex* mi) {
   LLIterator* iter;
   SearchResult* res;
   int qurey_len;
-  char** qurey = (char**)malloc(sizeof(char*) * LINE_SIZE);
+  char** qurey = (char**)calloc(0, sizeof(char*) * LINE_SIZE);
   Verify333(qurey != NULL);
 
   qurey_len = GetNextLine(stdin, qurey);

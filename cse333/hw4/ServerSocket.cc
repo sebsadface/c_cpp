b77793fe8e3ca12e06205cf6f058f4fe5fa42c36
Sebastian Liu <sebsadface@gmail.com>
2023-03-09 16:06:17 -0800

    server socket update

--- cse333/hw4/ServerSocket.cc
+++ cse333/hw4/ServerSocket.cc
@@ -22,8 +22,11 @@
 #include "./ServerSocket.h"
 
 extern "C" {
-  #include "libhw1/CSE333.h"
+#include "libhw1/CSE333.h"
 }
+
+using std::string;
+using std::to_string;
 
 namespace hw4 {
 
@@ -36,8 +39,7 @@ ServerSocket::~ServerSocket() {
   // Close the listening socket if it's not zero.  The rest of this
   // class will make sure to zero out the socket if it is closed
   // elsewhere.
-  if (listen_sock_fd_ != -1)
-    close(listen_sock_fd_);
+  if (listen_sock_fd_ != -1) close(listen_sock_fd_);
   listen_sock_fd_ = -1;
 }
 
@@ -48,8 +50,64 @@ bool ServerSocket::BindAndListen(int ai_family, int* const listen_fd) {
   // and set the ServerSocket data member "listen_sock_fd_"
 
   // STEP 1:
+  if (ai_family != AF_INET && ai_family != AF_INET6 && ai_family != AF_UNSPEC) {
+    return false;
+  }
 
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof(struct addrinfo));
+  hints.ai_family = ai_family;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+  hints.ai_flags |= AI_V4MAPPED;
+  hints.ai_protocol = IPPROTO_TCP;
+  hints.ai_canonname = nullptr;
+  hints.ai_addr = nullptr;
+  hints.ai_next = nullptr;
 
+  struct addrinfo* result;
+  string portnum = to_string(this->port_);
+
+  int res = getaddrinfo(nullptr, portnum.c_str(), &hints, &result);
+
+  if (res != 0) {
+    return false;
+  }
+
+  for (struct addrinfo* rp = result; rp != nullptr; rp = rp->ai_next) {
+    this->listen_sock_fd_ =
+        socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+
+    if (this->listen_sock_fd_ == -1) {
+      this->listen_sock_fd_ = -1;
+      continue;
+    }
+
+    int optval = 1;
+    setsockopt(this->listen_sock_fd_, SOL_SOCKET, SO_REUSEADDR, &optval,
+               sizeof(optval));
+
+    if (bind(this->listen_sock_fd_, rp->ai_addr, rp->ai_addrlen) == 0) {
+      this->sock_family_ = rp->ai_family;
+      break;
+    }
+
+    close(this->listen_sock_fd_);
+    this->listen_sock_fd_ = -1;
+  }
+
+  freeaddrinfo(result);
+
+  if (this->listen_sock_fd_ <= 0) {
+    return false;
+  }
+
+  if (listen(this->listen_sock_fd_, SOMAXCONN) != 0) {
+    close(this->listen_sock_fd_);
+    return false;
+  }
+
+  *listen_fd = this->listen_sock_fd_;
   return true;
 }
 
@@ -65,7 +123,65 @@ bool ServerSocket::Accept(int* const accepted_fd,
   // through the various output parameters.
 
   // STEP 2:
+  int client_fd;
+  struct sockaddr_storage caddr;
+  socklen_t caddr_len = sizeof(caddr);
+  while (1) {
+    client_fd = accept(this->listen_sock_fd_,
+                       reinterpret_cast<struct sockaddr*>(&caddr), &caddr_len);
+    if (client_fd < 0) {
+      if ((errno == EAGAIN) || (errno == EINTR)) {
+        continue;
+      }
+      close(this->listen_sock_fd_);
+      return false;
+    }
+    break;
+  }
 
+  *accepted_fd = client_fd;
+
+  if (caddr.ss_family == AF_INET) {
+    char astring[INET_ADDRSTRLEN];
+
+    struct sockaddr_in* sa4 = reinterpret_cast<struct sockaddr_in*>(&caddr);
+    inet_ntop(AF_INET, &(sa4->sin_addr), astring, INET_ADDRSTRLEN);
+    *client_addr = string(astring);
+    *client_port = htons(sa4->sin_port);
+  } else {
+    char astring[INET6_ADDRSTRLEN];
+
+    struct sockaddr_in6* sa6 = reinterpret_cast<struct sockaddr_in6*>(&caddr);
+    inet_ntop(AF_INET6, &(sa6->sin6_addr), astring, INET6_ADDRSTRLEN);
+    *client_addr = string(astring);
+    *client_port = htons(sa6->sin6_port);
+  }
+
+  char hname[1024];
+  hname[0] = '\0';
+  if (sock_family_ == AF_INET) {
+    struct sockaddr_in srvr;
+    socklen_t srvrlen = sizeof(srvr);
+    char addrbuf[INET_ADDRSTRLEN];
+    getsockname(client_fd, reinterpret_cast<struct sockaddr*>(&srvr), &srvrlen);
+    inet_ntop(AF_INET, &srvr.sin_addr, addrbuf, INET_ADDRSTRLEN);
+    getnameinfo(reinterpret_cast<struct sockaddr*>(&srvr), srvrlen, hname, 1024,
+                nullptr, 0, 0);
+
+    *server_addr = string(addrbuf);
+    *server_dns_name = string(hname);
+  } else {
+    struct sockaddr_in6 srvr;
+    socklen_t srvrlen = sizeof(srvr);
+    char addrbuf[INET6_ADDRSTRLEN];
+    getsockname(client_fd, reinterpret_cast<struct sockaddr*>(&srvr), &srvrlen);
+    inet_ntop(AF_INET6, &srvr.sin6_addr, addrbuf, INET6_ADDRSTRLEN);
+    getnameinfo(reinterpret_cast<struct sockaddr*>(&srvr), srvrlen, hname, 1024,
+                nullptr, 0, 0);
+
+    *server_addr = string(addrbuf);
+    *server_dns_name = string(hname);
+  }
 
   return true;
 }

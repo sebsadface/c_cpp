4225ef1222927626f50c5811dfc9939edd067c3b
Sebastian Liu <sebsadface@gmail.com>
2023-03-11 00:07:30 -0800

    HttpServer bug fixes

--- cse333/hw4/HttpUtils.cc
+++ cse333/hw4/HttpUtils.cc
@@ -58,6 +58,7 @@ bool IsPathSafe(const string& root_dir, const string& test_file) {
 
   if (realpath(root_dir.c_str(), dir_root_path) == nullptr ||
       realpath(test_file.c_str(), file_root_path) == nullptr) {
+    std::cout << "get real path failed" << endl;
     return false;
   }
 
